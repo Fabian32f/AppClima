e148d0aa71d370e151941a8c8f8d271bb21a9af6
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-03-05 22:28:09 -0500

    Appclima terminada

--- app/src/main/java/com/cbtis/appclima/Ciudades.kt
+++ app/src/main/java/com/cbtis/appclima/Ciudades.kt
@@ -15,12 +15,13 @@ class Ciudades : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
 
-        val bMexico = findViewById<Button>(R.id.bMexico)
+        val bMexico = findViewById<Button>(R.id.bMexico) //Shanghai
         val bBerlin = findViewById<Button>(R.id.bBerlin)
         val bChetumal = findViewById<Button>(R.id.bChetumal)
         val bCancun = findViewById<Button>(R.id.bCancun)
         val bMorelos = findViewById<Button>(R.id.bMorelos)
         val bTulum = findViewById<Button>(R.id.bTulum)
+        val bShanghai = findViewById<Button>(R.id.bShanghai)
 
 
         bMexico.setOnClickListener (View.OnClickListener {
@@ -37,25 +38,31 @@ class Ciudades : AppCompatActivity() {
 
         bChetumal.setOnClickListener (View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-chetumal")
+            intent.putExtra(TAG, "3531023")
             startActivity(intent)
         })
 
         bCancun.setOnClickListener (View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-cancun")
+            intent.putExtra(TAG, "3531673")
             startActivity(intent)
         })
 
         bMorelos.setOnClickListener (View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-morelos")
+            intent.putExtra(TAG, "3995398")
             startActivity(intent)
         })
 
         bTulum.setOnClickListener (View.OnClickListener {
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-tulum")
+            intent.putExtra(TAG, "3515040")
+            startActivity(intent)
+        })
+
+        bShanghai.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "1796236")
             startActivity(intent)
         })
 
